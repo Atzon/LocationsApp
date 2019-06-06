3dce0f48ee9f49b20d5fe53d67a48c5469ff65f9
kstopyra <kamil.stopyra95@gmail.com>
2019-06-06 15:54:57 +0200

    Added autocomplete feature

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import {Input} from 'antd';
+import {connect} from "react-redux";
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handlePlaceSelect = this.handlePlaceSelect.bind(this);
+    }
+
+    handlePlaceSelect() {
+        let place = this.autocomplete.getPlace();
+        if (place.geometry) {
+            this.props.map.setCenter(place.geometry.location);
+            this.props.map.setZoom(13);
+        }
+    }
+
+    componentDidMount() {
+        this.autocomplete = new window.google.maps.places.Autocomplete(
+            document.getElementById('autocomplete'));
+        this.autocomplete.addListener('place_changed', this.handlePlaceSelect);
+    }
+
     render() {
         return (
-            <Input placeholder={"Location..."}/>
-        );
+            <Input id="autocomplete" className="controls" type="text" placeholder="Location..."/>
+     );
     }
 }
+
+function mapStateToProps(state){
+    return{
+        map: state.map
+    };
+}
+
+export default connect(mapStateToProps, {})(SearchBar);
 
